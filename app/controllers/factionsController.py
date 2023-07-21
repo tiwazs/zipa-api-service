213665f44f97ccfca72f0bfdd4f2cb168e353e0d
kaiser24 <damsog38@gmail.com>
2023-07-21 14:52:40 -0500

    Factions CRUD

--- app/controllers/factionsController.py
+++ app/controllers/factionsController.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, Response, status
 
-from ..models.factionDTO import FactionCreateDTO
+from ..models.factionDTO import FactionCreateDTO, FactionUpdateDTO
 from ..services.factionsService import FactionService
 
 router = APIRouter(prefix="/factions", tags=["Factions"])
@@ -16,6 +16,7 @@ async def get_faction_by_id(id: str, request: Request, response: Response):
         if faction is None:
             response.status_code = status.HTTP_204_NO_CONTENT
             return {"error": "Faction not found"}
+        
         return faction
     except Exception as e:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -25,6 +26,32 @@ async def get_faction_by_id(id: str, request: Request, response: Response):
 async def create_faction(faction: FactionCreateDTO, request: Request, response: Response):
     try:
         return await FactionService(request.app.state.db).create(faction)
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"error": str(e)}
+    
+@router.put("/{id}")
+async def update_faction(id: str, faction: FactionUpdateDTO, request: Request, response: Response):
+    try:
+        faction = await FactionService(request.app.state.db).update(id, faction)
+        if faction is None:
+            response.status_code = status.HTTP_204_NO_CONTENT
+            return {"error": "Faction not found"}
+        
+        return faction
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"error": str(e)}
+
+@router.delete("/{id}")
+async def delete_faction(id: str, request: Request, response: Response):
+    try:
+        faction = await FactionService(request.app.state.db).delete(id)
+        if faction is None:
+            response.status_code = status.HTTP_204_NO_CONTENT
+            return {"error": "Faction not found"}
+        
+        return faction
     except Exception as e:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {"error": str(e)}
--- app/services/factionsService.py
+++ app/services/factionsService.py
@@ -10,13 +10,36 @@ class FactionService:
         return await self.database.faction.find_many()
 
     async def get_by_id(self, id: str) -> FactionDTO:
-        return await self.database.faction.find_unique( where={"id": id} )
+        return await self.database.faction.find_unique( 
+            where={"id": id} 
+        )
 
     async def create(self, faction: FactionCreateDTO) -> FactionDTO:
-        return await self.database.faction.create( data=faction.dict() )
+        return await self.database.faction.create( 
+            data=faction.dict() 
+        )
 
     async def update(self, id: str, faction: FactionDTO) -> FactionDTO:
-        return await self.database.faction.update({"id": id}, faction)
+        faction_dict = faction.dict()
+
+        # Get faction Data
+        faction_current = await self.database.faction.find_unique( 
+            where={"id": id} 
+        )
+        if(not faction_current): return None
+        faction_current_dict = faction_current.dict()
+
+        # If incomming data is empty, use current data
+        for key in faction_dict:
+            if faction_dict[key] is None:
+                faction_dict[key] = faction_current_dict[key]
+        
+        return await self.database.faction.update( 
+            where={"id": id}, 
+            data=faction_dict 
+        )
 
     async def delete(self, id: str) -> FactionDTO:
-        return await self.database.faction.delete({"id": id})
+        return await self.database.faction.delete(
+            where={"id": id}
+        )
