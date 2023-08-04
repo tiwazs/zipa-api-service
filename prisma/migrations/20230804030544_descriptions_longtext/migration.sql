af0da79f3bc5dc4c28e0f9657bbbeee08388d34c
kaiser24 <damsog38@gmail.com>
2023-08-03 22:06:29 -0500

    Descriptions are now of type text to allow long texts

---
+++ prisma/migrations/20230804030544_descriptions_longtext/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE `Effect` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Faction` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Item` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Skill` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `SkillType` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Trait` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Unit` MODIFY `description` TEXT NOT NULL;
